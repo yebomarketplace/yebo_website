d1f2f4c878193647bf6e9417edc94f9dca8f0323
Tyler <tjt28165@gmail.com>
2019-05-26 08:03:53 +0000

    changed site to app

--- site.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__, static_url_path='/static'))
-
-@app.route('/')
-def hello_world():
-    return render_template('YeboHomepage.html')
