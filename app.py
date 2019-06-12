b186f1f038eda7e73a53123c1b7bb9d684bc57ba
Tyler <tjt28165@gmail.com>
2019-06-12 09:09:18 +0000

    added a new page to the app part, this will be for viewing files

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/')
 def hello_world():
     return render_template('YeboHomepage.html')
+
+@app.route('/app')
+def hello_photos():
+    return render_template('YeboPhotoApp.html')
+#integrate this to do the photos
+hists = os.listdir('static/plots')
+hists = ['plots/' + file for file in hists]
+return render_template('report.html', hists = hists)
