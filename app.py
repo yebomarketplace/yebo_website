c9f14e40413ba7972eafbc6843f765aa2f1a16d8
Tyler Tresslar <47172450+ttresslar@users.noreply.github.com>
2019-06-12 21:21:36 +0300

    update app.py
    updated to reflect changes in the file structure, and to include username

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template
 import os, json
 
 def create_dict(d_name):
-    file = open('static/'+d_name+'/'+d_name+'.json', encoding='utf-8')
+    file = open('static/IGContent/'+d_name+'/'+d_name+'.json', encoding='utf-8')
     file = json.load(file)
     file = file["GraphImages"]
     d_info = []
@@ -11,10 +11,12 @@ def create_dict(d_name):
         url = os.path.basename(item['urls'][0]).split("?")[0]
         comment = item['edge_media_to_caption']['edges'][0]['node']['text']
         likes = str(item['edge_media_preview_like']['count'])
+        username = item['username']
         d_info.append({
-        'photo':d_name+'/'+url,
-        'comment':comment,
-        'likes':likes
+            'photo':'IGContent/'+d_name+'/'+url,
+            'comment':comment,
+            'likes':likes,
+            'username':username
         })
     return d_info
     
